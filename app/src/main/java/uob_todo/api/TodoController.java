52c92cef57f523eea5109cfbc2da6eb487241319
Ben Meier <1651305+AstromechZA@users.noreply.github.com>
2018-10-16 20:49:04 +0100

    Add  optional Scrypt request penalty in order to demonstrate autoscaling (#19)
    * Added security stuff with Scrypt cpu penalty on all API methods
    * Improved line count

--- app/src/main/java/uob_todo/api/TodoController.java
+++ app/src/main/java/uob_todo/api/TodoController.java
@@ -1,6 +1,8 @@
 package uob_todo.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import uob_todo.api.exceptions.BadRequestException;
 import uob_todo.api.exceptions.NotFoundException;
@@ -11,6 +13,19 @@ public class TodoController {
 
     private final TodoRepository todoSource;
 
+    @Value("${todoapp.penalty.factor}")
+    private int penaltyFactor;
+
+    /*
+    This function runs a cpu/memory penalty in order to generate some fake load. This is used to drive metrics
+    examples and demonstrate auto-scaling of pods/containers.
+     */
+    private void runPenaltyIfRequired() {
+        if (penaltyFactor > 0) {
+            new SCryptPasswordEncoder(1 << penaltyFactor, 8, 1, 32, 64).encode("");
+        }
+    }
+
     @Autowired
     public TodoController(TodoRepository todoSource){
         this.todoSource = todoSource;
@@ -18,16 +33,19 @@ public class TodoController {
 
     @GetMapping("/{id}")
     public TodoItem getTodo(@PathVariable("id") Long id) throws Exception {
+        this.runPenaltyIfRequired();
         return todoSource.findById(id).orElseThrow(() -> new NotFoundException("item not found"));
     }
 
     @GetMapping()
     public Iterable<TodoItem> listTodos() {
+        this.runPenaltyIfRequired();
         return todoSource.findAll();
     }
 
     @PostMapping()
     public TodoItem createTodo(@RequestBody TodoItem item) throws Exception {
+        this.runPenaltyIfRequired();
         if (item.getTitle().equals("")) {
             throw new BadRequestException("empty 'title'");
         }
@@ -36,6 +54,7 @@ public class TodoController {
 
     @PutMapping("/{id}")
     public TodoItem updateTodo(@PathVariable("id") Long id, @RequestBody TodoItem item) throws Exception {
+        this.runPenaltyIfRequired();
         if (item.getTitle().equals("")) {
             throw new BadRequestException("empty 'title'");
         }
