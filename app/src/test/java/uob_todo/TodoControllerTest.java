c2f7536de5bf0f20a05ca26c226fd60672a76d74
Ben Meier <ben.meier@oracle.com>
2018-10-16 23:02:11 +0100

    Don't need profile active in test

--- app/src/test/java/uob_todo/TodoControllerTest.java
+++ app/src/test/java/uob_todo/TodoControllerTest.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("unsecured")
 public class TodoControllerTest {
 
     @Autowired
